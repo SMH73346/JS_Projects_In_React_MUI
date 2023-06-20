ab382cd6135073b103c0103a1560edcdbff4a247
Syed Muhammad Hussain <SMh73346@gmail.com>
2023-06-20 14:27:02 +0500

    Color Flipper Created

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Header from "./component/header";
+import ColorFlipper from "./component/01_Color_Flipper/index";
+// import Counter from "./component/02_Counter/index";
+
+const App = () => {
+  return (
+    <>
+      <Header/>
+      <ColorFlipper/>
+      {/* <Counter/> */}
+    </>
+  )
+}
+
+export default App
